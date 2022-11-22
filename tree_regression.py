6f0c8cf31d9a0c57ba1455263c1072ae72bcc2c4
jon-robbins <jonathan.robbins@bse.eu>
2022-11-22 11:30:23 +0100

    Updates
    Finished bagging/boosting, started tweaking hyperparameters.

--- tree_regression.py
+++ tree_regression.py
@@ -79,5 +79,215 @@ plot_tree = plot_tree(clf_tree,
                       rounded=True, 
                       fontsize=14)
 
+#%%
+#Make decision tree regressor
+from sklearn.tree import DecisionTreeRegressor
+ 
+# Instantiate dt
+dt = DecisionTreeRegressor(max_depth=8,
+             min_samples_leaf=0.13,
+            random_state=3)
+ 
+# Fit dt to the training set
+dt.fit(X_train, y_train)
 
+#
+# 2. Evaluate your model
+#
+# Import mean_squared_error from sklearn.metrics as MSE
+from sklearn.metrics import mean_squared_error as MSE
+ 
+# Compute y_pred
+y_pred = dt.predict(X_test)
+ 
+# Compute mse_dt
+mse_dt = MSE(y_test, y_pred)
+ 
+# Print rmse_dt
+print("Test set MSE of dt: {:.2f}".format(mse_dt))
+#%%
+from sklearn.neighbors import KNeighborsClassifier as KNN
+from sklearn.tree import DecisionTreeClassifier
+
+# Set seed for reproducibility
+SEED=3
+ 
+# Instantiate knn
+knn = KNN(n_neighbors=5)
+ 
+# Instantiate tree
+tree = DecisionTreeClassifier(min_samples_leaf = 0.13, random_state=SEED)
+ 
+# Define the list classifiers
+classifiers = [('K Nearest Neighbours', knn), ('Classifier Tree', tree)]
+
+
+#
+# Evaluate individual classifiers
+#
+from sklearn.metrics import accuracy_score
+ 
+# Iterate over the pre-defined list of classifiers
+for clf_name, clf in classifiers:    
+  
+    # Fit clf to the training set
+    clf.fit(X_train, y_train)    
+
+    # Predict y_pred
+    y_pred = clf.predict(X_test)
+     
+    # Calculate mse
+    accuracy = accuracy_score(y_test, y_pred) 
+    
+    # Evaluate clf's accuracy on the test set
+    print('{:s} : {:.3f}'.format(clf_name, accuracy))
+#%%
+#Now let's check if we can get a higher accuracy rating with ensembling
+
+# Import VotingClassifier from sklearn.ensemble
+from sklearn.ensemble import VotingClassifier
+ 
+# Instantiate a VotingClassifier vc
+vc = VotingClassifier(estimators=classifiers)     
+ 
+# Fit vc to the training set
+vc.fit(X_train, y_train)   
+ 
+# Evaluate the test set predictions
+y_pred = vc.predict(X_test)
+ 
+# Calculate accuracy score
+accuracy = accuracy_score(y_test, y_pred)
+print('Voting Classifier: {:.3f}'.format(accuracy))
+#Nope, doesn't seem like this will give us a better result. 
+#%%
+#Bagging
+# Import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier
+ 
+# Import BaggingClassifier
+from sklearn.ensemble import BaggingClassifier
+ 
+# Instantiate dt
+dt = DecisionTreeClassifier(random_state=1)
+ 
+# Instantiate bc
+bc = BaggingClassifier(base_estimator=dt, n_estimators=50, random_state=1)
+
+# Fit bc to the training set
+bc.fit(X_train, y_train)
+ 
+# Predict test set labels
+y_pred = bc.predict(X_test)
+ 
+# Evaluate acc_test
+acc_test = accuracy_score(y_test, y_pred)
+print('Test set accuracy of bc: {:.2f}'.format(acc_test)) 
+#Get 1 more percent with bagging. 
+#%%
+# Instantiate rf
+rf = RandomForestClassifier(max_depth=9, random_state=0)
+             
+# Fit rf to the training set    
+rf.fit(X_train, y_train) 
+ 
+# Predict test set labels
+y_pred = rf.predict(X_test)
+ 
+# Evaluate acc_test
+acc_test = accuracy_score(y_test, y_pred)
+print('Test set accuracy of rf: {:.2f}'.format(acc_test)) 
+#RF gets 93%
+#%%
+
+# Predict the test set labels
+y_pred = rf.predict(X_test)
+ 
+# Evaluate the test set RMSE
+rmse_test = MSE(y_test, y_pred) ** 0.5
+ 
+# Print rmse_test
+print('Test set RMSE of rf: {:.2f}'.format(rmse_test))
+#RMSE is 0.26 up to this point. Not great.
+#%%
+#Let's rank feature importance now.
+# Create a pd.Series of features importances
+importances = pd.Series(data=rf.feature_importances_,
+                        index= features.columns)
+ 
+# Sort importances
+importances_sorted = importances.sort_values()
+ 
+# Draw a horizontal barplot of importances_sorted
+importances_sorted.plot(kind='barh', color='lightgreen')
+plt.title('Features Importances')
+plt.show()
+
+#DayMins and MonthlyCharge are the most important feature, plus the daily_call_length feature I added. 
+#%%
+#Now let's try using AdaBoost to see if we can improve RMSE
+# Import AdaBoostClassifier
+from sklearn.ensemble import AdaBoostRegressor
+ 
+ada_reg = AdaBoostRegressor(n_estimators=100)
+
+# Fit ada to the training set
+ada_reg.fit(X_train, y_train)
+ 
+# Compute the probabilities of obtaining the positive class
+y_pred_ada = ada_reg.predict(X_test)
+
+# Evaluate the test set RMSE
+rmse_test = MSE(y_test, y_pred_ada) ** 0.5
+ 
+# Print rmse_test
+print('Test set RMSE of ada: {:.2f}'.format(rmse_test))
+#lol it's worse than with bagging, 0.29 compared to 0.26
+#%%
+#Now let's try XGBoost
+import xgboost as xgb
+
+xgb_model = xgb.XGBRegressor(objective="reg:squarederror", random_state=1)
+
+# Fit ada to the training set
+xgb_model.fit(X_train, y_train)
+
+# Compute the probabilities of obtaining the positive class
+y_pred_xg = xgb_model.predict(X_test)
+
+# Evaluate the test set RMSE
+rmse_test = MSE(y_test, y_pred_xg) ** 0.5
+ 
+# Print rmse_test
+print('Test set RMSE of xgboost: {:.2f}'.format(rmse_test))
+#still no better than with just rf, 0.26
+#%%
+#Hyperparameter tuning
+# Instantiate rf
+rf_2 = RandomForestClassifier(max_depth=2, random_state=0)
+             
+# Fit rf to the training set    
+rf_2.fit(X_train, y_train) 
+ 
+# Predict test set labels
+y_pred = rf_2.predict(X_test)
+ 
+# Evaluate acc_test
+acc_test = accuracy_score(y_test, y_pred)
+print('Test set accuracy of rf with max_depth_2: {:.4f}'.format(acc_test)) 
+
+
+# Instantiate rf
+rf_12 = RandomForestClassifier(max_depth=12, random_state=0)
+             
+# Fit rf to the training set    
+rf_12.fit(X_train, y_train) 
+ 
+# Predict test set labels
+y_pred = rf_12.predict(X_test)
+ 
+# Evaluate acc_test
+acc_test = accuracy_score(y_test, y_pred)
+print('Test set accuracy of rf with max_depth_7: {:.4f}'.format(acc_test)) 
+#Ideal depth is at 12, gives us 93.5% accuracy
 
